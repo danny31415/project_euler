110997af45187b0c9a20ab55e6cd7faed3d9c1e3
Danny Lynch <danny31415@gmail.com>
2012-01-07 04:53:45 -0600

    Set null list default for command line params Fix up the library name references

--- src/euler_problems.py
+++ src/euler_problems.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from optparse import OptionParser
 import __main__
+import sys
 
 import problem1
 import problem2
@@ -8,15 +9,19 @@ import problem3
 import problem4
 import problem5
 
+# need option to print to file instead of stdout
+
 def main(argv=None):
     parser = OptionParser()
-    parser.add_option("-p", "--problem-num", dest="problems",help="problem number to run", action='append')
-    (options, args) = parser.parse_args()
+    parser.add_option("-p", "--problem-num", dest="problems", action='append',
+                      help="problem number to run", default=[])
+    (options, args) = parser.parse_args(argv)
 
     for prob in options.problems:
-        func_name = 'problem' + prob
-        p = getattr(__main__, func_name)
-        print(func_name + ': ' + str(p.main()))
+        lib_name = 'problem' + prob
+        lib = globals()[lib_name]
+        print(lib_name + ': ' + str(lib.main()))
 
 if __name__ == "__main__":
     main()
+
